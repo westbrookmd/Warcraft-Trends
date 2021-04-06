fdb1667bfa4fd54ed77b528732d455b432e93990
westbrookmd <71563811+westbrookmd@users.noreply.github.com>
2021-04-06 19:19:08 -0400

    Incorporate Data from BugTrackerv2
    Incorporated my own data set (pulled from and reformatted for parsing - https://github.com/SimCMinMax/WoW-BugTracker/issues )

--- src/charts/views.py
+++ src/charts/views.py
@@ -8,8 +8,21 @@ from rest_framework.response import Response
 
 User = get_user_model()
 Bugs = 40
-
-
+number = [725,724,722,721,720,719,718,717,716,715,714,713,712,711,709,708,705,702,701,700,699,698,697,696,694,689]
+author = []
+state = []
+group_class = ["Death Knight","Rogue: Subtlety""Priest: Shadow","Rogue: Assassination",
+"Rogue: Assassination","Warlock: Affliction","Rogue: Subtlety",
+"Hunter","Priest","Rogue","Death Knight","Death Knight","Mage","Hunter","Paladin","Rogue","Mage",
+"Death Knight","Priest","Rogue","Priest","Mage","Mage","Mage","Priest","Shaman"]
+priests = sum('Priest' in s for s in group_class)
+Hunters = sum('Hunter' in s for s in group_class)
+Rogues = sum('Rogue' in s for s in group_class)
+Warlock = sum('Warlock' in s for s in group_class)
+Death_Knights = sum('Death Knight' in s for s in group_class)
+Mages = sum('Mage' in s for s in group_class)
+Shamans = sum('Shaman' in s for s in group_class)
+bug_count = len(number)
 
 class HomeView(View):
     def get(self, request, *args, **kwargs):
@@ -31,8 +44,8 @@ class ChartData(APIView):
 
     def get(self, request, format=None):
         qs_count = User.objects.all().count()
-        labels = ["Bugs", "Total Users", "Yellow", "Green", "Purple", "Orange"]
-        default_items = [Bugs, qs_count, 2, 3, 12, 2]
+        labels = ["Total Bugs", "Priests", "Hunters", "Rogues", "Warlocks", "Death Knights", "Mages", "Shamans"]
+        default_items = [bug_count, priests, Hunters, Rogues, Warlock, Death_Knights, Mages, Shamans]
         data = {
                 "labels": labels,
                 "default": default_items,
