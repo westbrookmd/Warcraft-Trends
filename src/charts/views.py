b3ddee62e183fe5ae0a309345d240831b02e38b9
westbrookmd <71563811+westbrookmd@users.noreply.github.com>
2021-04-20 19:07:50 -0400

    Change Site Design

--- src/charts/views.py
+++ src/charts/views.py
@@ -2,9 +2,12 @@ from django.contrib.auth import get_user_model
 from django.http import JsonResponse
 from django.shortcuts import render
 from django.views.generic import View
-
 from rest_framework.views import APIView
 from rest_framework.response import Response
+import ezsheets
+
+
+
 
 User = get_user_model()
 Bugs = 40
